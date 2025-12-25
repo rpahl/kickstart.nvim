00eee66826951ea86c01abfbbf79589115d347ac
Roman Pahl <roman.x.pahl@gsk.com>
2025-12-25 22:48:36 +0100

    add custom keymaps for better movement and window resize

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,10 +26,16 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<A-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<A-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<A-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<A-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Make adjusting split sizes easy
+vim.keymap.set('n', '<C-A-h>', '<cmd>vertical resize +3<CR>', { silent = true })
+vim.keymap.set('n', '<C-A-l>', '<cmd>vertical resize -3<CR>', { silent = true })
+vim.keymap.set('n', '<C-A-k>', '<cmd>resize +3<CR>',          { silent = true })
+vim.keymap.set('n', '<C-A-j>', '<cmd>resize -3<CR>',          { silent = true })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -44,11 +50,19 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+desc = 'Highlight when yanking (copying) text',
+group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+  vim.hl.on_yank()
   end,
-})
+  })
 
--- vim: ts=2 sts=2 sw=2 et
+
+-- vim: ts=2 sts=2 sw=2 etvertical
+
+-- Move around quicker
+vim.keymap.set({ 'n', 'x', 'o' }, '<C-j>', '4j', { silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, '<C-k>', '4k', { silent = true })
+vim.keymap.set('n', '<A-j>', '4<C-e>', { silent = true })
+vim.keymap.set('n', '<A-k>', '4<C-y>', { silent = true })
+
