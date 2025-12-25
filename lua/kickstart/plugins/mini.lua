30e2b7f14ae7d5bb870d41eed90f4b41e5e93ac0
Roman Pahl <roman.x.pahl@gsk.com>
2025-12-25 22:50:39 +0100

    remap mini keys

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -15,7 +15,17 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = 'gsa',
+          delete = 'gsd',
+          find = 'gsf',
+          find_left = 'gsF',
+          highlight = 'gsh',
+          replace = 'gsr',
+          update_n_lines = 'gsn',
+        },
+      })
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
