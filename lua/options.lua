4e051f90ad7151dc851e34a9dc6304758ee97990
Roman Pahl <roman.x.pahl@gsk.com>
2025-12-25 22:46:33 +0100

    customize vim options to expand tabs as spaces and add wildignore defs

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -60,9 +60,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
--- Show which line your cursor is on
-vim.o.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
@@ -70,5 +67,10 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+vim.o.expandtab = true
 
 -- vim: ts=2 sts=2 sw=2 et
+vim.opt.wildignore:append(
+  [[*/tmp/*]], '*.swp', '*.zip', '*.exe', '*.o', '*.obj', '.git'
+)
+
