d91e3808ef9981b91b12ff40b96284e40651e5ea
Roman Pahl <roman.pahl@gmail.com>
2025-12-31 13:19:41 +0100

    activate nerd fonts

--- init.lua
+++ init.lua
@@ -88,10 +88,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 require 'options'
@@ -99,6 +99,8 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 require 'autocmds'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
@@ -106,6 +108,3 @@ require 'lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
