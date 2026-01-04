ea67e8fa5e7b9c72841cd71af9252e0e2453ed20
Roman Pahl <roman.pahl@gmail.com>
2026-01-04 19:14:13 +0100

    deactivate auto-format on save

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -1,19 +1,3 @@
-local has_words_before = function()
-    local col = vim.api.nvim_win_get_cursor(0)[2]
-    if col == 0 then
-        return false
-    end
-    local line = vim.api.nvim_get_current_line()
-    return line:sub(col, col):match '%s' == nil
-end
-
--- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
-local trigger_cmp = function(cmp)
-    if has_words_before() then
-        return cmp.insert_next()
-    end
-end
-
 return {
     { -- Autocompletion
         'saghen/blink.cmp',
@@ -71,29 +55,12 @@ return {
                 -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
                 -- <c-e>: Hide menu
                 -- <c-k>: Toggle signature help
-                -- See :h blink-cmp-config-keymap for defining your own keymap
-                preset = 'none',
-
-                -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-                ['<C-d>'] = { 'show', 'show_documentation', 'hide_documentation' },
-                ['<C-A-d>'] = { 'show', 'show_documentation', 'hide_documentation' },
-                ['<Up>'] = { 'scroll_documentation_up', 'fallback' },
-                ['<Down>'] = { 'scroll_documentation_down', 'fallback' },
-                ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
-                -- ['<C-k>'] = false,
-                -- <C-K> and <C-J> is already covered by general vim.keymap.set('c', ...) - see keymaps.lua
-                ['<C-e>'] = { 'hide', 'fallback' },
                 --
-                -- ['<Tab>'] = { 'show_and_insert', 'fallback' },
-                ['<CR>'] = { 'accept', 'fallback' },
-                ['<Tab>'] = { 'select_next', 'fallback' },
-                -- ['<Tab>'] = { 'show', 'select_next', 'fallback' },
-                -- completion = {
-                --     menu = { enabled = false },
-                --     list = { selection = { preselect = false }, cycle = { from_top = false } },
-                -- }
-                -- ['<C-b>'] = false,
-                -- ['<C-f>'] = false,
+                -- See :h blink-cmp-config-keymap for defining your own keymap
+                preset = 'default',
+
+                ['<C-s>'] = { 'show', 'show_signature', 'hide_signature', 'fallback' },
+                ['<C-d>'] = { 'show', 'show_documentation', 'hide_documentation', 'fallback' },
                 -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
                 --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
             },
@@ -103,23 +70,20 @@ return {
                 nerd_font_variant = 'mono',
             },
 
+            completion = {
+                -- By default, you may press `<c-space>` to show the documentation.
+                -- Optionally, set `auto_show = true` to show the documentation after a delay.
+                documentation = { auto_show = false, auto_show_delay_ms = 200 },
+            },
+
             sources = {
-                default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'lsp', 'path', 'snippets', 'lazydev' },
                 providers = {
-                    lazydev = {
-                        module = 'lazydev.integrations.blink',
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
-                        score_offset = 100,
-                    },
-                    cmdline = {
-                        -- ignores cmdline completions when executing shell commands
-                        enabled = function()
-                            return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():match "^[%%0-9,'<>%-]*!"
-                        end,
-                    },
+                    lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
                 },
             },
-            --
+            snippets = { preset = 'luasnip' },
+
             -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
             -- which automatically downloads a prebuilt binary when enabled.
             --
@@ -127,60 +91,10 @@ return {
             -- the rust implementation via `'prefer_rust_with_warning'`
             --
             -- See :h blink-cmp-config-fuzzy for more information
-            -- fuzzy = { implementation = 'lua' },
-            fuzzy = {
-                implementation = 'prefer_rust_with_warning',
-                sorts = {
-                    'score', -- Primary sort: by fuzzy matching score
-                    'sort_text', -- Secondary sort: by sortText field if scores are equal
-                    'label', -- Tertiary sort: by label if still tied
-                },
-            },
-            --
+            fuzzy = { implementation = 'lua' },
 
             -- Shows a signature help window while you type arguments for a function
             signature = { enabled = true },
-            completion = {
-                menu = { auto_show = false },
-                documentation = { auto_show = false },
-                signature = { auto_show = false },
-                ghost_text = { enabled = true },
-                list = { selection = { preselect = false } },
-                -- keyword = { range = 'prefix' },
-            },
-            cmdline = {
-                keymap = {
-                    -- ['<Tab>'] = { 'show', 'accept' },
-                    -- ['<Tab>'] = { trigger_cmp, 'fallback' },
-                    -- ['<Tab>'] = { 'show_and_insert_or_accept_single', 'fallback' },
-                    ['<Tab>'] = { 'show', 'select_next', 'fallback' },
-                    ['<CR>'] = { 'accept_and_enter', 'fallback' },
-                },
-                completion = {
-                    -- menu = { auto_show = false },
-                    list = { selection = { preselect = false } },
-                    menu = {
-                        auto_show = function(ctx, _)
-                            return ctx.mode == 'cmdwin'
-                        end,
-                    },
-                    ghost_text = { enabled = true },
-                },
-                sources = function()
-                    local type = vim.fn.getcmdtype()
-                    -- Search forward and backward
-                    if type == '/' or type == '?' then
-                        return { 'buffer' }
-                    end
-                    -- Commands
-                    if type == ':' or type == '@' then
-                        return { 'cmdline', 'buffer' }
-                    end
-                    return {}
-                end,
-                fuzzy = { sorts = { 'exact', 'score', 'score_text' } },
-            },
-            snippets = { preset = 'luasnip' },
         },
     },
 }
