503ddbf9cdcb4f9a6426cd3fa977a06695d62ead
Roman Pahl <roman.pahl@gmail.com>
2026-01-04 18:24:05 +0100

    revert back to lua default of two char indent

--- lua/options.lua
+++ lua/options.lua
@@ -65,4 +65,4 @@ opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Completely ignore file modelines like: vim: ts=2 sts=2 sw=2 et
 -- as it overrides the global shiftwidth and tabstop option values
-opt.modeline = false
+-- opt.modeline = false
