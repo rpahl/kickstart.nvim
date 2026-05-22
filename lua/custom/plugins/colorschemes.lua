81e5e213dd677d59ee24432baf4717eea7df4f9e
Roman Pahl <roman.x.pahl@gsk.com>
2026-05-22 09:15:37 +0200

    set colorscheme solarized as default

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -7,6 +7,12 @@ return {
     { "shaunsingh/moonlight.nvim" },
     --]]
     { "craftzdog/solarized-osaka.nvim" },
-    { "maxmx03/solarized.nvim" },
     { "marko-cerovac/material.nvim" },
+    {
+        "maxmx03/solarized.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme 'solarized'
+        end,
+    },
 }
